6c8283bbc6acac3ea6b083c8090219ee6ecd71a0
Hieu-Nguyen1031 <159960044+Hieu-Nguyen1031@users.noreply.github.com>
2025-10-12 02:46:29 -0400

    Update synch.c

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -148,8 +148,7 @@ sema_up (struct semaphore *sema)
   old_level = intr_disable ();
   if (!list_empty (&sema->waiters)){
      list_sort(&sema->waiters, thread_priority_comparison. NULL);      //Sorting waiters based on their priority
-     thread_unblock (list_entry (list_pop_front (&sema->waiters),      
-                                struct thread, elem));
+     thread_unblock (list_entry (list_pop_front (&sema->waiters), struct thread, elem));
   }
    
   sema->value++;
