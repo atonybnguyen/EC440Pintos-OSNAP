c5d4f030399f064f97f8e8205b4b2c3256ab7945
Hieu-Nguyen1031 <159960044+Hieu-Nguyen1031@users.noreply.github.com>
2025-10-13 00:06:44 -0400

    Update synch.c

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -303,6 +303,12 @@ lock_acquire (struct lock *lock)
   ASSERT (!intr_context ());
   ASSERT (!lock_held_by_current_thread (lock));
 
+   if (lock->holder != NULL){
+      donate_priority(lock);
+   }
+   //Once we acquire the lock, it is no longer waiting for the lock
+   thread_current()->waiting_on = NULL;
+   
   sema_down (&lock->semaphore);
   lock->holder = thread_current ();
 }
@@ -338,6 +344,9 @@ lock_release (struct lock *lock)
   ASSERT (lock != NULL);
   ASSERT (lock_held_by_current_thread (lock));
 
+   //Remove the priority donation
+   remove_priority(lock);
+   
   lock->holder = NULL;
   sema_up (&lock->semaphore);
 }
